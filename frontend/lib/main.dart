be60f098874b8f1e9205866a17f7c926c9faf96a
AdamSaafy <adamsaafy5@gmail.com>
2023-06-05 15:15:32 +0100

    changes

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -111,7 +111,7 @@ class _LoginPageState extends State<LoginPage> {
   final List<Widget> _screens = [
     HomePage(),
     SignInScreen(),
-    LoginScreen(),// screenThree LoginScreen just to test the page 
+    ScreenThree(),// LoginScreen() LoginScreen just to test the page 
     ScreenFour(),
   ];
   @override
