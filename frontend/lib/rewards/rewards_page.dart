176fc1c13c635604c8718717dcb8c998605047f9
fredChargui <117441187+fredChargui@users.noreply.github.com>
2023-06-23 20:56:12 +0000

    done

--- frontend/lib/rewards/rewards_page.dart
+++ frontend/lib/rewards/rewards_page.dart
@@ -2,20 +2,20 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flare_flutter/flare_controller.dart';
-import 'reward_item_widget.dart';
 import 'controller.dart';
+import 'reward_item_widget.dart';
+
 class RewardPage extends StatefulWidget {
   @override
   _RewardPageState createState() => _RewardPageState();
 }
-
 
 class _RewardPageState extends State<RewardPage> {
   late List<dynamic> rewardData = [];
   bool loading = true;
   final String apiUrl = dotenv.env["API_URL"]!;
   final CustomFlareController flareController = CustomFlareController();
+  
   Future<void> fetchRewardData() async {
     final response = await http.get(Uri.parse('$apiUrl/rewards'));
 
@@ -26,6 +26,28 @@ class _RewardPageState extends State<RewardPage> {
       });
     } else {
       throw Exception('Failed to load reward data');
+    }
+  }
+
+  Future<void> spendPoints(String userId, int pointsToSpend) async {
+    try {
+      final response = await http.put(
+        Uri.parse('$apiUrl/rewards/$userId/spend-points'),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: json.encode({'points': pointsToSpend}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        print(data);
+      } else {
+        final error = json.decode(response.body);
+        print('Error spending points: $error');
+      }
+    } catch (error) {
+      print('Error making request: $error');
     }
   }
 
@@ -49,7 +71,11 @@ class _RewardPageState extends State<RewardPage> {
                 final reward = rewardData[index];
                 return RewardItemWidget(
                   rewardItem: RewardItem.fromJson(reward),
-                  onTap: () {},
+                  onTap: () {
+                    String userId = "user-id"; // Replace this with the actual user ID
+                    int pointsToSpend = reward['price'];
+                    spendPoints(userId, pointsToSpend);
+                  },
                   flareController: flareController,
                 );
               },
